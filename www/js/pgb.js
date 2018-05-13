71292dc0b31799520698e9520b489594a6cd23a1
Garret29 <szymon.piotrowski93@gmail.com>
2018-05-13 17:15:26 +0200

    fix

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -2,14 +2,9 @@ const app = angular.module('MeetApp', []);
 
 app.controller('controller', function ($scope) {
 
+    $scope.beep = function (time) {
+        navigator.notification.beep(time)
+    }
+
 });
-
-function init() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-}
-
-function onDeviceReady() {
-    navigator.notification.beep(2);
-    deviceInfo();
-}
 
