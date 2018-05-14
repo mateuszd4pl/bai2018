b44baae63ed34e696e83f11437e373e605f78469
Garret29 <szymon.piotrowski93@gmail.com>
2018-05-14 19:34:12 +0200

    testing cordova; added firebase support

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,11 +1,23 @@
 const app = angular.module('MeetApp', []);
 
+app.service('storageService', function () {
+
+});
+
 app.controller('controller', function ($scope) {
     $scope.text = "test";
 
     $scope.test = function () {
-        $scope.text = "tested";
-    }
+        $scope.text = test("xD")
+    };
 
 });
+
+function init() {
+    document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+    navigator.notification.beep(1);
+}
 
