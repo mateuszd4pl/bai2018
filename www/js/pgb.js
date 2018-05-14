dd1cb3ae4fa21945ef892a9f9fb22eb1ec82df1e
Garret29 <xseventythreex@gmail.com>
2018-05-14 18:22:37 +0200

    merge
    * Update index.html
    
    * added angularjs support

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,15 +1,11 @@
 const app = angular.module('MeetApp', []);
 
 app.controller('controller', function ($scope) {
+    $scope.text = "test";
+
+    $scope.test = function () {
+        $scope.text = "tested";
+    }
 
 });
-
-function init() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-}
-
-function onDeviceReady() {
-    navigator.notification.beep(2);
-    deviceInfo();
-}
 
