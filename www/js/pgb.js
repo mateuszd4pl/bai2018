4dbdab9eafbbfaddb0be2aa6ccac429f726399f2
Garret29 <szymon.piotrowski93@gmail.com>
2018-05-14 19:50:49 +0200

    test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -8,10 +8,28 @@ app.controller('controller', function ($scope) {
     $scope.text = "test";
 
     $scope.test = function () {
-        $scope.text = test("xD")
+        $scope.text = test();
+        deviceInfo();
     };
 
 });
+
+function deviceInfo() {
+
+    info =  'Hi, I am your smartphone :-)' + '\n' +
+        '=====' + '\n' +
+        'Device Name    : '     + device.name     + '\n' +
+        'Device Cordova : '  + device.cordova + '\n' +
+        'Device Platform: ' + device.platform + '\n' +
+        'Device UUID    : '     + device.uuid     + '\n' +
+        'Device Model   : '    + device.model     + '\n' +
+        'Device Version : '  + device.version  + '\n';
+
+    navigator.notification.alert(info);
+
+}
+
+let test = () => {return "xD"};
 
 function init() {
     document.addEventListener("deviceready",onDeviceReady, false);
@@ -20,4 +38,3 @@ function init() {
 function onDeviceReady() {
     navigator.notification.beep(1);
 }
-
