e1a8c98309afdb0bda9fb191c71ddcaa4436b6d0
Garret29 <szymon.piotrowski93@gmail.com>
2018-05-19 14:13:45 +0200

    test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,40 +1,40 @@
 const app = angular.module('MeetApp', []);
 
-app.service('storageService', function () {
+app.service('userStorageService', function () {
+    this.db = firebase.database();
 
+     this.saveUser = async (user)=>{
+         try {
+             let pushRef = this.db.ref().child('users').push();
+             let key = pushRef.key;
+             this.db.ref('users/'+key).set(user);
+             return key;
+         } catch (e) {
+             console.log(e)
+         }
+    }
 });
 
-app.controller('controller', function ($scope) {
-    $scope.text = "test";
+app.controller('controller', function ($scope, userStorageService) {
+    $scope.user = {};
 
-    $scope.test = function () {
-        $scope.text = test();
-        deviceInfo();
-    };
-
+    $scope.saveUser = async function saveUser(user) {
+        $scope.user.id = userStorageService.saveUser(user).then(()=>{console.log($scope.user)});
+    }
 });
 
-function deviceInfo() {
 
-    info =  'Hi, I am your smartphone :-)' + '\n' +
-        '=====' + '\n' +
-        'Device Name    : '     + device.name     + '\n' +
-        'Device Cordova : '  + device.cordova + '\n' +
-        'Device Platform: ' + device.platform + '\n' +
-        'Device UUID    : '     + device.uuid     + '\n' +
-        'Device Model   : '    + device.model     + '\n' +
-        'Device Version : '  + device.version  + '\n';
-
-    navigator.notification.alert(info);
-
-}
-
-let test = () => {return "xD"};
+// function User(mail, name, lastname, phone) {
+//     this.mail = mail;
+//     this.name = name;
+//     this.lastname = lastname;
+//     this.phone = phone;
+// }
 
 function init() {
     document.addEventListener("deviceready",onDeviceReady, false);
 }
 
 function onDeviceReady() {
-    navigator.notification.beep(1);
+
 }
