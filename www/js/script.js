134d492e7292dd1a217df258697afcd81867c8c0
Kamil <kamil.przybycien@gmail.com>
2018-06-16 18:34:08 +0200

    test

--- www/js/script.js
+++ www/js/script.js
@@ -6,7 +6,9 @@ $(document).on("pagecreate","#pageone",function(){
 
 var options = {
     date: new Date(),
-    mode: 'date'
+    mode: 'date',
+    is24Hour: 'true',
+    androidTheme: "THEME_HOLO_LIGHT",
 };
 
 function onSuccess(date) {
