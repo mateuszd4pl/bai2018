e261ab3f519ab586571d3c10b11e551a182697f0
Kamil <kamil.przybycien@gmail.com>
2018-06-16 18:19:02 +0200

    add new event view

--- www/js/script.js
+++ www/js/script.js
@@ -3,3 +3,16 @@ $(document).on("pagecreate","#pageone",function(){
         window.location.href = "#mainGroup";
     });
 });
+
+var options = {
+    date: new Date(),
+    mode: 'date'
+};
+
+function onSuccess(date) {
+    alert('Selected date: ' + date);
+}
+
+function onError(error) { // Android only
+    alert('Error: ' + error);
+}
