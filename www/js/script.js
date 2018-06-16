39a9eaeb82a2b4fc372c8053d9aa5cb01bd413f4
Kamil <kamil.przybycien@gmail.com>
2018-06-16 19:26:11 +0200

    data

--- www/js/script.js
+++ www/js/script.js
@@ -4,17 +4,3 @@ $(document).on("pagecreate","#pageone",function(){
     });
 });
 
-var options = {
-    date: new Date(),
-    mode: 'date',
-    is24Hour: 'true',
-    androidTheme: "THEME_HOLO_LIGHT",
-};
-
-function onSuccess(date) {
-    alert('Selected date: ' + date);
-}
-
-function onError(error) { // Android only
-    alert('Error: ' + error);
-}
